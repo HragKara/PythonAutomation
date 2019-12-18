3e8244dbe37abee9ae23f7574608070ceaf0b20c
HragKara <46092072+HragKara@users.noreply.github.com>
2019-12-18 14:00:49 -0800

    Updated create.py
    Removed token and added comment

--- create.py
+++ create.py
@@ -4,7 +4,7 @@ import os
 
 path = "/Users/hragmacbookpro/Desktop/Projects"
 
-g = Github('a5c0a555e8db77526c6a2de594df988c9223a90e')
+g = Github('{{ **** add your github token *****}}')
 
 def create():
     #~~~~~~~~~~~~ Creates Folder ~~~~~~~~~~~~~~~
@@ -12,6 +12,7 @@ def create():
     check = path + "/" + str(folderName)
     if os.path.isdir(check):
         print("\n\n~~~~~~~~~~Folder Name is taken Try Again!~~~~~~~~~~\nIgnore Below\n\n")
+        # Need to find out how to end the shell script after this so it wont proceed to push ot github.
         return
     else:
         os.makedirs(path + "/" + str(folderName))
