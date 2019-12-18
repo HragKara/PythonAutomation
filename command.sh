d4be6bbe9d264589fdfe697c23eaf17203c7a3d6
HragKara <46092072+HragKara@users.noreply.github.com>
2019-12-18 13:57:10 -0800

    Updated Shell script
    Forgot to add this file

--- command.sh
+++ command.sh
@@ -1,2 +1,18 @@
-#!/bin/sh
-python create.py
+#!/bin/bash
+function create(){
+    cd Desktop/my_environments {{should be path to your environments folder}}
+    source Pygithub/bin/activate {{activate your env}}
+    cd ~
+    cd Desktop/Projects/PythonProjectAutomation
+    python create.py $1
+    cd ~
+    cd Desktop/Projects/$1
+    git init
+    git remote add origin https://github.com/{{Github UserName}}/$1.git
+    touch README.md
+    git add .
+    git commit -m "Initial Commit"
+    git push -u origin master
+    code .
+    
+}
